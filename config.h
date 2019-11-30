1522f5a7283eaf0271d1e8a9085abd88e1a3e69f
joel <hi@joelbrewster.com>
2019-12-01 00:17:07 +1100

    grid, spiral, dwindle layout

--- config.h
+++ config.h
@@ -1,9 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 #include "selfrestart.c"
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int gappx     = 0;       /* gaps between windows on tiling mode*/
+static const unsigned int gappx     = 16;       /* gaps between windows on tiling mode*/
 static const unsigned int snap      = 20;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 16;  /* systray spacing */
@@ -12,18 +13,21 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 6;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 6;        /* vertical padding for statusbar */
-static const char *fonts[]          = { "monospace:size=12" };
+static const int vertpadbar         = 8;        /* vertical padding for statusbar */
+static const char *fonts[]           = {
+                                        "monospace:size=12:antialias=true:autohint=true",
+                                        "siji:size=12:antialias=true:autohint=true"
+};
 static const char dmenufont[]       = "monospace:size=13";
-static const char col_gray1[]       = "#14161b";
-static const char col_gray2[]       = "#22252c";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_white[]       = "#ffffff";
-static const char col_blue[]        = "#5294E2";
+static const char col_background[]  = "#222222";
+static const char col_mid1[]        = "#4f4f4f";
+static const char col_mid2[]        = "#aeaeae";
+static const char col_foreground[]  = "#fcfcfc";
+static const char col_highlight[]   = "#3498d8";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_white, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_white, col_blue,  col_blue  },
+                                       /*               fg         bg         border   */
+                                       [SchemeNorm] = { col_foreground, col_background, col_background },
+                                       [SchemeSel]  = { col_foreground, col_highlight,  col_highlight  },
 };
 
 /* tagging */
@@ -48,11 +52,13 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-                                 /* symbol     arrange function */
-                                 { "[T]",      tile },    /* first entry is default */
-                                 { "[F]",      NULL },    /* no layout function means floating behavior */
-                                 { "[M]",      monocle },
-                                 { "[G]",      grid },
+                                 /* symbol  arrange function */
+                                 { "",     tile },    /* first entry is default */
+                                 { "",     NULL },    /* no layout function means floating behavior */
+                                 { "",      monocle },
+                                 { "",     grid },
+                                 { "",     spiral },
+                                 { "",     dwindle },
 };
 
 /* key definitions */
@@ -68,7 +74,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_blue, "-sf", col_white, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_background, "-nf", col_mid2, "-sb", col_highlight, "-sf", col_foreground, NULL };
 static const char *dmenuexcmd[]    = { "dmenu_extended_run", NULL };
 static const char *termcmd[]       = { "alacritty", NULL };
 static const char *webbrowsecmd[]  = { "firefox", NULL };
@@ -80,6 +86,14 @@ static const char *passwordscmd[]  = { "bitwarden", NULL };
 static const char *lockcmd[]       = { "physlock", NULL };
 static const char *networkcmd[]    = { "nm-connection-editor", NULL };
 static const char *screenshotcmd[] = { "xfce4-screenshooter", NULL };
+static const char *pavucontrol[]   = { "pavucontrol", NULL };
+static const char *mutecmd[]       = { "amixer", "-q", "set", "Master", "toggle", NULL };
+static const char *volupcmd[]      = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
+static const char *voldowncmd[]    = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
+static const char *brupcmd[]       = { "sudo", "xbacklight", "-inc", "10", NULL };
+static const char *brdowncmd[]     = { "sudo", "xbacklight", "-dec", "10", NULL };
+
+
 
 
 static Key keys[] = {
@@ -94,22 +108,25 @@ static Key keys[] = {
                      { MODKEY|ShiftMask,             XK_m,      spawn,          {.v = evolutioncmd } },
                      { MODKEY|ShiftMask,             XK_n,      spawn,          {.v = networkcmd } },
                      { MODKEY|ShiftMask,             XK_p,      spawn,          {.v = photoscmd } },
-                     { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = webbrowsecmd } },
                      { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = screenshotcmd } },
+                     { MODKEY|ShiftMask,             XK_v,      spawn,          {.v = pavucontrol } },
+                     { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = webbrowsecmd } },
                      { MODKEY,                       XK_b,      togglebar,      {0} },
+                     { MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
                      { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
                      { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
                      { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-                     { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+                     { MODKEY,                       XK_o,      incnmaster,     {.i = -1 } },
                      { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
                      { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
                      { MODKEY,                       XK_Return, zoom,           {0} },
-                     { MODKEY,                       XK_Tab,    view,           {0} },
                      { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
                      { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
                      { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
                      { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
                      { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
+                     { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[4]} },
+                     { MODKEY,                       XK_d,      setlayout,      {.v = &layouts[5]} },
                      { MODKEY,                       XK_space,  setlayout,      {0} },
                      { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
                      { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -133,6 +150,11 @@ static Key keys[] = {
                      TAGKEYS(                        XK_9,                      8)
                      { MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
                      { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+                     { 0, XF86XK_AudioMute, spawn, {.v = mutecmd } },
+                     { 0, XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
+                     { 0, XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
+                     { 0, XF86XK_MonBrightnessUp, spawn, {.v = brupcmd} },
+                     { 0, XF86XK_MonBrightnessDown, spawn, {.v = brdowncmd} },
 };
 
 /* button definitions */
